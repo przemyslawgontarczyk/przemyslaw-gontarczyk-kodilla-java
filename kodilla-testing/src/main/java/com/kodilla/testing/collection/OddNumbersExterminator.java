502fd0219f420dc1512172c9b85034983fd5fadd
karol <88lkarol@gmail.com>
2017-11-12 03:03:32 +0100

    poprawki 6.3

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,22 +1,21 @@
 package com.kodilla.testing.collection;
 
 
+import java.util.ArrayList;
 
-import java.util.*;
+public class OddNumbersExterminator {//uzywaj wbudowane narzedzia do formatowania ctrl-alt-L
 
-public class OddNumbersExterminator {
+    ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
 
-    ArrayList <Integer> exterminate(ArrayList<Integer> numbers){
+        for (int i = 0; i < numbers.size(); i++) {
 
-    for ( int i=0; i<numbers.size(); i++){
+            if (numbers.get(i) % 2 == 0) {
+            } else {
+                numbers.remove(i);
+            }
+        }
 
-       if (numbers.get(i) %2 ==0){
-       }
-       else{numbers.remove(i);
-       }
-    }
-
-        return  numbers ;
+        return numbers;
 
 
     }
