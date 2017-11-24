c00052fc66cc15249c4a7880712926fba4482274
“przemyslawgontarczyk <przemyslawgontarczyk@gmail.com>
2017-11-24 16:18:25 +0100

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -25,5 +26,8 @@ public class StreamMain {
         poemBeautifier.beautify("male litery staja sie duze",PoemBeautifier::uPerCase);
         poemBeautifier.beautify("To bedzie ciekawostka, bo utnie nam zdanie od 10 litery",PoemBeautifier::cutFromTen);
         poemBeautifier.beautify("amazing all a are e now!",PoemBeautifier::replecAllATOE);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
