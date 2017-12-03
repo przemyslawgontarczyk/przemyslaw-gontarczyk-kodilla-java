e45d41f325b6de8f5e302cbf11db0dbddfd70411
przemyslawgontarczyk <przemyslawgontarczyk@gmail.com>
2017-12-03 13:48:28 +0100

    9.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreVideoList.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreVideoList {
+
+    public static void main(String args[]) {
+
+        String videoResult = MovieStore.getMovies().entrySet()
+                .stream()
+                .flatMap(books -> books.getValue().stream())
+                .map(String::toString)
+                .collect(Collectors.joining("!", "", ""));
+        System.out.print(videoResult);
+
+    }
+}
