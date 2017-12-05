14fde4eb50e75e0a067f4b20100534f77641de78
“przemyslawgontarczyk <przemyslawgontarczyk@gmail.com>
2017-12-06 00:10:12 +0100

    9.4.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/newSupplier/Supplier.java
@@ -0,0 +1,8 @@
+package com.kodilla.good.patterns.newSupplier;
+
+import java.util.Map;
+
+public class gitSupplier {
+    private String supplierName;
+
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/newSupplier/SupplyCheckForOrder.java
@@ -0,0 +1,4 @@
+package com.kodilla.good.patterns.newSupplier;
+
+public class SupplyCheckForOrder {
+}
