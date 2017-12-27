6fd04f091a6fca0c220c1d7d3f79714d1c480936
przemyslawgontarczyk <przemyslawgontarczyk@gmail.com>
2017-12-27 19:06:48 +0100

    10.4

--- kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -1,4 +1,4 @@
-package main.java.com.kodilla.spring.web;
+package com.kodilla.spring.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
